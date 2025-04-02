13deff537a9c6235ae162d2728e87f23410d6f69
Sanjay Siva <realsanjaysiva@gmail.com>
2025-04-03 00:06:56 +0530

    Added constraints 1 and 2
    1. Each cell in puzzle contains at least one value.
    2. Each cell in the puzzle contains at most one value.

--- solver.py
+++ solver.py
@@ -6,4 +6,25 @@ with open('p.txt', 'r') as file:
     puzzle = list(line)
 
 grid = np.array(puzzle).reshape(9, 9)
-print(grid)
+
+constructor = list()
+
+constraint1 = list()
+for i in range(1, 10):
+    for j in range(1, 10):
+        for x in range(1, 10):
+            constructor.append(100*i+10*j+x)
+        constraint1.append(constructor)
+        constructor = []
+
+constraint2 = list()
+for i in range(1, 10):
+    for j in range(1, 10):
+        for selected_x in range(1, 10):
+            for x in range(1, 10):
+                if x == selected_x:
+                    constructor.append(100*i+10*j+x)
+                else:
+                    constructor.append(-(100*i+10*j+x))
+            constraint2.append(constructor)
+            constructor = []

b52a5e84c4873bbda4c96f0f3612dae6fc2bc5fb
Sanjay Siva <realsanjaysiva@gmail.com>
2025-04-04 16:39:39 +0530

    added write to output file

--- solver.py
+++ solver.py
@@ -65,15 +65,27 @@ for row_group in range(0, 3):
 fixed_constraints = constraint1 + constraint2 + constraint3 + constraint4 + constraint5
 
 with open('p.txt', 'r') as file:
-    line = file.readline().replace('\n', '')
+    lines = file.readlines()
 
-puzzle = Sudoku(line)
+with open('output.txt', 'w') as out_file:
+    pass
 
-clauses  = fixed_constraints + puzzle.variable_constraint
+for line in lines:
+    puzzle = Sudoku(line.replace('\n', ''))
 
-pycosat_solution = pycosat.solve(clauses)
-solution = puzzle.format_pycosat_solution(pycosat_solution)
+    clauses  = fixed_constraints + puzzle.variable_constraint
+
+    pycosat_solution = pycosat.solve(clauses)
+    solution = puzzle.format_pycosat_solution(pycosat_solution)
     
+    with open('output.txt', 'a') as out_file:
+        out_string = ''
+        for row in range(9):
+            for column in range(9):
+                out_string += str(solution[row][column])
+        out_string += '\n'
+        out_file.write(out_string)
 
-for array in solution:
-    print(array)
+    # for array in solution:
+    #     print(array)
+    # print()
